5df8dc56314416dfe8378b12bfea9006e9fe2310
andradeju <julliana.andrade08@gmail.com>
2023-12-08 23:13:21 -0300

    :recycle: refactor: Melhorando indentação dos arquivos

--- projeto_pets_home/animais/models.py
+++ projeto_pets_home/animais/models.py
@@ -1,24 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Animal(models.Model):
-  GENEROS_ANIMAIS = (
-    ('F', 'Fêmea'),
-    ('M', 'Macho')
-  )
-  nome = models.CharField(max_length=55)
-  genero = models.CharField(max_length=50, choices=GENEROS_ANIMAIS, null=True)
-  especie = models.CharField(max_length=50)
-  idade = models.PositiveIntegerField()
-  peso = models.PositiveIntegerField(null=True)
-  raca = models.CharField(max_length=50)
-  historico_saude = models.TextField()
-  imagem = models.ImageField(upload_to='imagens/')
+    GENEROS_ANIMAIS = (
+        ('F', 'Fêmea'),
+        ('M', 'Macho')
+    )
+    nome = models.CharField(max_length=55)
+    genero = models.CharField(
+        max_length=50, choices=GENEROS_ANIMAIS, null=True)
+    especie = models.CharField(max_length=50)
+    idade = models.PositiveIntegerField()
+    peso = models.PositiveIntegerField(null=True)
+    raca = models.CharField(max_length=50)
+    historico_saude = models.TextField()
+    imagem = models.ImageField(upload_to='imagens/')
 
-  def __str__(self):
-    return f'{self.nome} - {self.especie} : {self.idade}'
+    def __str__(self):
+        return f'{self.nome} - {self.especie} : {self.idade}'
 
-  class Meta:
-    verbose_name = 'Cadastro de Animal' 
-    verbose_name_plural = 'Cadastros de Animais'
-    ordering = ['-idade']
+    class Meta:
+        verbose_name = 'Cadastro de Animal'
+        verbose_name_plural = 'Cadastros de Animais'
+        ordering = ['-idade']
--- projeto_pets_home/base/models.py
+++ projeto_pets_home/base/models.py
@@ -2,27 +2,34 @@ from django.db import models
 from animais.models import Animal
 
 # Create your models here.
+
+
 class AnimalAdocao(models.Model):
-  animal = models.ForeignKey(Animal, on_delete=models.CASCADE, null=True, blank=True)
-  nome = models.CharField(max_length=51)
-  email = models.EmailField(max_length=77)
-  cpf = models.CharField(max_length=11)
-  dataNascimento = models.DateField()
-  telefone = models.CharField(max_length=16)
-  profissao = models.CharField(max_length=100)
-  endereco = models.CharField(max_length=255) 
-  cidade = models.CharField(max_length=102)
-  estado = models.CharField(max_length=2)
-  cep = models.CharField(max_length=9)
-  motivoAdocao = models.TextField()
-  emCasoDeViagem = models.TextField()
-  experienciaAnterior = models.TextField()
-  temAnimal = models.BooleanField(choices=[(True, 'Sim'), (False, 'Não')], default=False)
-  status = models.CharField(max_length=22, choices=[('Pendente', 'Pendente'), ('Aprovada', 'Aprovada'), ('Recusada', 'Recusada')], default='Pendente')
-  data = models.DateTimeField(auto_now_add=True)
-  def __str__(self):
-    return f'{self.nome} [{self.cpf}]'
-  class Meta:
-    verbose_name = 'Formulário de Adoção'
-    verbose_name_plural = 'Formulários de Adoções'
-    ordering = ['-data']
+    animal = models.ForeignKey(
+        Animal, on_delete=models.CASCADE, null=True, blank=True)
+    nome = models.CharField(max_length=51)
+    email = models.EmailField(max_length=77)
+    cpf = models.CharField(max_length=11)
+    dataNascimento = models.DateField()
+    telefone = models.CharField(max_length=16)
+    profissao = models.CharField(max_length=100)
+    cep = models.CharField(max_length=8)
+    endereco = models.CharField(max_length=255)
+    cidade = models.CharField(max_length=102)
+    estado = models.CharField(max_length=2)
+    motivoAdocao = models.TextField()
+    emCasoDeViagem = models.TextField()
+    experienciaAnterior = models.TextField()
+    temAnimal = models.BooleanField(
+        choices=[(True, 'Sim'), (False, 'Não')], default=False)
+    status = models.CharField(max_length=22, choices=[('Pendente', 'Pendente'), (
+        'Aprovada', 'Aprovada'), ('Recusada', 'Recusada')], default='Pendente')
+    data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.nome} [{self.cpf}]'
+
+    class Meta:
+        verbose_name = 'Formulário de Adoção'
+        verbose_name_plural = 'Formulários de Adoções'
+        ordering = ['-data']
