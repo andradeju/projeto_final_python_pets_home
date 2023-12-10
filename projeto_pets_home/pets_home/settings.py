9ab4d1b052959f05a99c6c0c4181af305b64a789
andradeju <julliana.andrade08@gmail.com>
2023-12-10 20:00:12 -0300

    :wrench: chore Atualizando o settings.py

--- projeto_pets_home/pets_home/settings.py
+++ projeto_pets_home/pets_home/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,12 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r0y57c1#)_6v0tcy9x!6)x5)rj8w0f9=&uqfpf1nkqla4@)2hj'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DJANGO_DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
